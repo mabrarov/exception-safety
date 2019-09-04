e5bb0a5e5e326a253ec98f8516d6055d541cf1ab
Marat Abrarov <abrarov@gmail.com>
2019-09-04 12:40:40 +0300

    Fixed permissions for Maven Wrapper launch script and added CI builds with Maven Wrapper

--- scripts/appveyor/build.ps1
+++ scripts/appveyor/build.ps1
@@ -1,4 +1,10 @@
-$build_cmd = "mvn -f ""${env:APPVEYOR_BUILD_FOLDER}\pom.xml"" --batch-mode clean package"
+if (${env:MAVEN_WRAPPER} -ne 0) {
+  $build_cmd = "& ""${env:APPVEYOR_BUILD_FOLDER}\mvnw.cmd"""
+} else {
+  $build_cmd = "mvn"
+}
+
+$build_cmd = "${build_cmd} -f ""${env:APPVEYOR_BUILD_FOLDER}\pom.xml"" --batch-mode clean package"
 
 if (${env:COVERAGE_BUILD} -ne 0) {
   $build_cmd = "${build_cmd} -P jacoco"
