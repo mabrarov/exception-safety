0effe9ac9dd26ebda2285c6c834921196b72bd33
Marat Abrarov <abrarov@gmail.com>
2020-08-26 01:23:58 +0300

    Fixed JDK 7 build on AppVeyor CI

--- scripts/appveyor/build.ps1
+++ scripts/appveyor/build.ps1
@@ -10,6 +10,10 @@ if (${env:COVERAGE_BUILD} -ne 0) {
   $build_cmd = "${build_cmd} -P jacoco"
 }
 
+if ((Test-Path env:MAVEN_BUILD_OPTIONS) -and (${env:MAVEN_BUILD_OPTIONS} -ne "")) {
+  $build_cmd = "${build_cmd} ${env:MAVEN_BUILD_OPTIONS}"
+}
+
 Write-Host "Building with: ${build_cmd}"
 Invoke-Expression "${build_cmd}"
 if (${LastExitCode} -ne 0) {
