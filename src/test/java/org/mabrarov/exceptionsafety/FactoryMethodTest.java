57d603a6dd3ebff7d38641bb75efa6333176c6dc
Marat Abrarov <abrarov@gmail.com>
2021-08-23 12:38:51 +0300

    Minor warning suppressed.

--- src/test/java/org/mabrarov/exceptionsafety/FactoryMethodTest.java
+++ src/test/java/org/mabrarov/exceptionsafety/FactoryMethodTest.java
@@ -135,7 +135,8 @@ public class FactoryMethodTest {
 
   private interface ResourceConfigurator {
 
-    void configureResource(final OutputStream resource) throws TestResourceConfigurationException;
+    void configureResource(@SuppressWarnings("unused") final OutputStream resource)
+        throws TestResourceConfigurationException;
 
   }
 
