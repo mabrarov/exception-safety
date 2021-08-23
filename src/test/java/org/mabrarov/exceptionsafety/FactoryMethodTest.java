622b9c03a03e144b1f2ff2efa8650edaa767a151
Ekaterina Tsareva <ekaterinatsr@gmail.com>
2021-08-23 16:27:30 +0300

    fix2 added

--- src/test/java/org/mabrarov/exceptionsafety/FactoryMethodTest.java
+++ src/test/java/org/mabrarov/exceptionsafety/FactoryMethodTest.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
@@ -32,193 +33,212 @@ import org.mockito.ArgumentMatchers;
 
 public class FactoryMethodTest {
 
-  @Rule
-  public TemporaryFolder temporaryFolder = new TemporaryFolder();
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
-  private final AtomicBoolean resourceClosed = new AtomicBoolean();
-  private ResourceCreator resourceCreator;
-  private ResourceConfigurator resourceConfigurator;
+    private final AtomicBoolean resourceClosed = new AtomicBoolean();
+    private ResourceCreator resourceCreator;
+    private ResourceConfigurator resourceConfigurator;
 
-  private OutputStream createConfiguredResource()
-      throws IOException, TestResourceConfigurationException {
-    final OutputStream resource = createResource();
-    configureResource(resource);
-    return resource;
-  }
+    private OutputStream createConfiguredResource()
+            throws IOException, TestResourceConfigurationException {
+        OutputStream resource = null;
+        Throwable throwable = null;
+        try {
+            resource = createResource();
+            configureResource(resource);
+            return resource;
+        } catch (final IOException | TestResourceConfigurationException | RuntimeException e) {
+            throwable = e;
+            throw e;
+        } catch (final Throwable e) {
+            throwable = e;
+            throw new AssertionError("Should never come here", e);
+        } finally {
+            if (throwable != null && resource != null) {
+                try {
+                    resource.close();
+                } catch (Throwable e) {
+                    throwable.addSuppressed(e);
+                }
 
-  @Test
-  public void test_noExceptions() {
-    resourceConfigurator = resourceConfiguratorNoException;
-    resourceCreator = resourceCreatorCloseNoException;
-    verifyResourceCreationAndClosing();
-  }
-
-  @Test
-  public void test_configurationCheckedException() {
-    resourceConfigurator = resourceConfiguratorCheckedException;
-    resourceCreator = resourceCreatorCloseNoException;
-    verifyResourceCreationAndClosing();
-  }
-
-  @Test
-  public void test_configurationRuntimeException() {
-    resourceConfigurator = resourceConfiguratorRuntimeException;
-    resourceCreator = resourceCreatorCloseNoException;
-    verifyResourceCreationAndClosing();
-  }
-
-  @Test
-  public void test_closeCheckedException() {
-    resourceConfigurator = resourceConfiguratorNoException;
-    resourceCreator = resourceCreatorCloseCheckedException;
-    verifyResourceCreationAndClosing();
-  }
-
-  @Test
-  public void test_closeRuntimeException() {
-    resourceConfigurator = resourceConfiguratorNoException;
-    resourceCreator = resourceCreatorCloseRuntimeException;
-    verifyResourceCreationAndClosing();
-  }
-
-  @Test
-  public void test_configurationCheckedException_closeCheckedException() {
-    resourceConfigurator = resourceConfiguratorCheckedException;
-    resourceCreator = resourceCreatorCloseCheckedException;
-    verifyResourceCreationAndClosing();
-  }
-
-  @Test
-  public void test_configurationCheckedException_closeRuntimeException() {
-    resourceConfigurator = resourceConfiguratorCheckedException;
-    resourceCreator = resourceCreatorCloseRuntimeException;
-    verifyResourceCreationAndClosing();
-  }
-
-  @Test
-  public void test_configurationRuntimeException_closeCheckedException() {
-    resourceConfigurator = resourceConfiguratorRuntimeException;
-    resourceCreator = resourceCreatorCloseCheckedException;
-    verifyResourceCreationAndClosing();
-  }
-
-  @Test
-  public void test_configurationRuntimeException_closeRuntimeException() {
-    resourceConfigurator = resourceConfiguratorRuntimeException;
-    resourceCreator = resourceCreatorCloseRuntimeException;
-    verifyResourceCreationAndClosing();
-  }
-
-  @Test
-  public void test_exceptionSuppressionError() {
-    resourceConfigurator = resourceConfiguratorRuntimeExceptionWithSuppressionFailure;
-    resourceCreator = resourceCreatorCloseRuntimeException;
-    verifyResourceCreationAndClosing();
-  }
-
-  private void verifyResourceCreationAndClosing() {
-    try (final OutputStream resource = createConfiguredResource();
-        final PrintStream printStream = new PrintStream(resource)) {
-      Assert.assertFalse("Resource should be opened", resourceClosed.get());
-      printStream.println("Test");
-    } catch (final Exception | TestSuppressionError e) {
-      e.printStackTrace();
-    }
-    Assert.assertTrue("Resource should be closed", resourceClosed.get());
-  }
-
-  private interface ResourceCreator {
-
-    OutputStream createResource(final File file) throws IOException;
-
-  }
-
-  private interface ResourceConfigurator {
-
-    void configureResource(@SuppressWarnings("unused") final OutputStream resource)
-        throws TestResourceConfigurationException;
-
-  }
-
-  private final ResourceCreator resourceCreatorCloseNoException = new ResourceCreator() {
-    @Override
-    public OutputStream createResource(final File file) throws IOException {
-      return new FileOutputStream(file) {
-        @Override
-        public void close() throws IOException {
-          super.close();
-          resourceClosed.set(true);
+            }
         }
-      };
     }
-  };
 
-  private final ResourceCreator resourceCreatorCloseCheckedException = new ResourceCreator() {
-    @Override
-    public OutputStream createResource(final File file) throws IOException {
-      return new FileOutputStream(file) {
-        @Override
-        public void close() throws IOException {
-          super.close();
-          resourceClosed.set(true);
-          throw new TestResourceCloseException();
+        @Test
+        public void test_noExceptions () {
+            resourceConfigurator = resourceConfiguratorNoException;
+            resourceCreator = resourceCreatorCloseNoException;
+            verifyResourceCreationAndClosing();
         }
-      };
-    }
-  };
 
-  private final ResourceCreator resourceCreatorCloseRuntimeException = new ResourceCreator() {
-    @Override
-    public OutputStream createResource(final File file) throws IOException {
-      return new FileOutputStream(file) {
-        @Override
-        public void close() throws IOException {
-          super.close();
-          resourceClosed.set(true);
-          throw new TestResourceCloseRuntimeException();
+        @Test
+        public void test_configurationCheckedException () {
+            resourceConfigurator = resourceConfiguratorCheckedException;
+            resourceCreator = resourceCreatorCloseNoException;
+            verifyResourceCreationAndClosing();
         }
-      };
-    }
-  };
 
-  private final ResourceConfigurator resourceConfiguratorNoException = new ResourceConfigurator() {
-    @Override
-    public void configureResource(final OutputStream resource) {
-    }
-  };
+        @Test
+        public void test_configurationRuntimeException () {
+            resourceConfigurator = resourceConfiguratorRuntimeException;
+            resourceCreator = resourceCreatorCloseNoException;
+            verifyResourceCreationAndClosing();
+        }
 
-  private final ResourceConfigurator resourceConfiguratorCheckedException = new ResourceConfigurator() {
-    @Override
-    public void configureResource(final OutputStream resource)
-        throws TestResourceConfigurationException {
-      throw new TestResourceConfigurationException();
-    }
-  };
+        @Test
+        public void test_closeCheckedException () {
+            resourceConfigurator = resourceConfiguratorNoException;
+            resourceCreator = resourceCreatorCloseCheckedException;
+            verifyResourceCreationAndClosing();
+        }
 
-  private final ResourceConfigurator resourceConfiguratorRuntimeException = new ResourceConfigurator() {
-    @Override
-    public void configureResource(final OutputStream resource) {
-      throw new TestRuntimeException();
-    }
-  };
+        @Test
+        public void test_closeRuntimeException () {
+            resourceConfigurator = resourceConfiguratorNoException;
+            resourceCreator = resourceCreatorCloseRuntimeException;
+            verifyResourceCreationAndClosing();
+        }
 
-  private final ResourceConfigurator resourceConfiguratorRuntimeExceptionWithSuppressionFailure = new ResourceConfigurator() {
-    @Override
-    public void configureResource(final OutputStream resource) {
-      final TestRuntimeException configurationException = spy(new TestRuntimeException());
-      doThrow(new TestSuppressionError()).when(configurationException)
-          .addSuppressed(ArgumentMatchers.<Throwable>any());
-      throw configurationException;
-    }
-  };
+        @Test
+        public void test_configurationCheckedException_closeCheckedException () {
+            resourceConfigurator = resourceConfiguratorCheckedException;
+            resourceCreator = resourceCreatorCloseCheckedException;
+            verifyResourceCreationAndClosing();
+        }
 
-  private OutputStream createResource() throws IOException {
-    return resourceCreator.createResource(temporaryFolder.newFile());
-  }
+        @Test
+        public void test_configurationCheckedException_closeRuntimeException () {
+            resourceConfigurator = resourceConfiguratorCheckedException;
+            resourceCreator = resourceCreatorCloseRuntimeException;
+            verifyResourceCreationAndClosing();
+        }
 
-  private void configureResource(final OutputStream resource)
+        @Test
+        public void test_configurationRuntimeException_closeCheckedException () {
+            resourceConfigurator = resourceConfiguratorRuntimeException;
+            resourceCreator = resourceCreatorCloseCheckedException;
+            verifyResourceCreationAndClosing();
+        }
+
+        @Test
+        public void test_configurationRuntimeException_closeRuntimeException () {
+            resourceConfigurator = resourceConfiguratorRuntimeException;
+            resourceCreator = resourceCreatorCloseRuntimeException;
+            verifyResourceCreationAndClosing();
+        }
+
+        @Test
+        public void test_exceptionSuppressionError () {
+            resourceConfigurator = resourceConfiguratorRuntimeExceptionWithSuppressionFailure;
+            resourceCreator = resourceCreatorCloseRuntimeException;
+            verifyResourceCreationAndClosing();
+        }
+
+        private void verifyResourceCreationAndClosing () {
+            try (final OutputStream resource = createConfiguredResource();
+                 final PrintStream printStream = new PrintStream(resource)) {
+                Assert.assertFalse("Resource should be opened", resourceClosed.get());
+                printStream.println("Test");
+            } catch (final Exception | TestSuppressionError e) {
+                e.printStackTrace();
+            }
+            Assert.assertTrue("Resource should be closed", resourceClosed.get());
+        }
+
+        private interface ResourceCreator {
+
+            OutputStream createResource(final File file) throws IOException;
+
+        }
+
+        private interface ResourceConfigurator {
+
+            void configureResource(@SuppressWarnings("unused") final OutputStream resource)
+                    throws TestResourceConfigurationException;
+
+        }
+
+        private final ResourceCreator resourceCreatorCloseNoException = new ResourceCreator() {
+            @Override
+            public OutputStream createResource(final File file) throws IOException {
+                return new FileOutputStream(file) {
+                    @Override
+                    public void close() throws IOException {
+                        super.close();
+                        resourceClosed.set(true);
+                    }
+                };
+            }
+        };
+
+        private final ResourceCreator resourceCreatorCloseCheckedException = new ResourceCreator() {
+            @Override
+            public OutputStream createResource(final File file) throws IOException {
+                return new FileOutputStream(file) {
+                    @Override
+                    public void close() throws IOException {
+                        super.close();
+                        resourceClosed.set(true);
+                        throw new TestResourceCloseException();
+                    }
+                };
+            }
+        };
+
+        private final ResourceCreator resourceCreatorCloseRuntimeException = new ResourceCreator() {
+            @Override
+            public OutputStream createResource(final File file) throws IOException {
+                return new FileOutputStream(file) {
+                    @Override
+                    public void close() throws IOException {
+                        super.close();
+                        resourceClosed.set(true);
+                        throw new TestResourceCloseRuntimeException();
+                    }
+                };
+            }
+        };
+
+        private final ResourceConfigurator resourceConfiguratorNoException = new ResourceConfigurator() {
+            @Override
+            public void configureResource(final OutputStream resource) {
+            }
+        };
+
+        private final ResourceConfigurator resourceConfiguratorCheckedException = new ResourceConfigurator() {
+            @Override
+            public void configureResource(final OutputStream resource)
+                    throws TestResourceConfigurationException {
+                throw new TestResourceConfigurationException();
+            }
+        };
+
+        private final ResourceConfigurator resourceConfiguratorRuntimeException = new ResourceConfigurator() {
+            @Override
+            public void configureResource(final OutputStream resource) {
+                throw new TestRuntimeException();
+            }
+        };
+
+        private final ResourceConfigurator resourceConfiguratorRuntimeExceptionWithSuppressionFailure = new ResourceConfigurator() {
+            @Override
+            public void configureResource(final OutputStream resource) {
+                final TestRuntimeException configurationException = spy(new TestRuntimeException());
+                doThrow(new TestSuppressionError()).when(configurationException)
+                        .addSuppressed(ArgumentMatchers.<Throwable>any());
+                throw configurationException;
+            }
+        };
+
+        private OutputStream createResource () throws IOException {
+            return resourceCreator.createResource(temporaryFolder.newFile());
+        }
+
+        private void configureResource ( final OutputStream resource)
       throws TestResourceConfigurationException {
-    resourceConfigurator.configureResource(resource);
-  }
+            resourceConfigurator.configureResource(resource);
+        }
 
-}
+    }
