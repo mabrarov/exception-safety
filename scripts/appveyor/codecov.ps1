58d3fa3d52344ffc1dbb22ba1a0dd4e565501f98
Marat Abrarov <abrarov@gmail.com>
2025-02-10 00:39:07 +0300

    Codecov for JDK 17 and 21 CI/CD builds.

--- scripts/appveyor/codecov.ps1
+++ scripts/appveyor/codecov.ps1
@@ -2,7 +2,7 @@ if (${env:COVERAGE_BUILD} -ne 0) {
   $codecov_root_folder = ${env:APPVEYOR_BUILD_FOLDER} -replace "\\", "/"
   $codecov_coverage_file = "${codecov_root_folder}/target/site/jacoco/jacoco.xml"
   Write-Host "Sending coverage data to Codecov"
-  appveyor-retry codecov --required --token "${env:CODECOV_TOKEN}" --file "${codecov_coverage_file}" --root "${codecov_root_folder}" -X gcov;
+  appveyor-retry codecov --required --token "${env:CODECOV_TOKEN}" --file "${codecov_coverage_file}" --flags "${env:CODECOV_FLAG}" --root "${codecov_root_folder}" -X gcov;
   if (${LastExitCode} -ne 0) {
     throw "Failed to send coverage data to Codecov"
   }
